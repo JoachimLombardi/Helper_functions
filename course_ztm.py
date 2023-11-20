625cff7aa96ffd0579fa6fe08e2abd1c06a07593
Joachim Lombardi <62808587+JoachimLombardi@users.noreply.github.com>
2023-11-20 17:18:05 +0100

    Update course_ztm.py

--- course_ztm.py
+++ course_ztm.py
@@ -524,3 +524,9 @@ def plot_train_test_decision_boundary(model:torch.nn.Module, X_train:torch.Tenso
     plt.title("Test")
     plot_decision_boundary(model, X_test, y_test)
 
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
